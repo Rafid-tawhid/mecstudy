82fff48f8de6b233ec8142cf021d8d96db891fb5
rafid-tawhid <rafidtawhid@gmail.com>
2024-12-10 23:47:06 +0600

    remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mecstudygroup/SplashScreen.dart';
@@ -7,12 +6,6 @@ import 'dart:core';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // try {
-  //   await Firebase.initializeApp();
-  //   print("Firebase initialized successfully!");
-  // } catch (e) {
-  //   print("Failed to initialize Firebase: $e");
-  // }
   runApp(MySplashScreen());
 }
 
